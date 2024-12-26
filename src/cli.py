c090f4bbf7d15c215f8a3822d294980e42095e7c
Abel Tomás <abeltomasr98@gmail.com>
2024-12-26 16:33:13 -0300

    0.16.4 - Colors for 'summary_expenses' messages

--- src/cli.py
+++ src/cli.py
@@ -9,7 +9,7 @@ from commands.budget import set_budget, delete_budget, budget
 
 
 @click.group()
-@click.version_option(version="0.16.3", prog_name="Expense Tracker CLI")
+@click.version_option(version="0.16.4", prog_name="Expense Tracker CLI")
 def cli():
     pass
 
